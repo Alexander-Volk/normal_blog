2a8ec0f21ce2351db9373ba67136bb927b2aec18
AlexanderVolk <mr.vol4onok@gmail.com>
2020-01-11 22:15:23 +0300

    Displayed a form for creating tags

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
 from blog.models import Post, Tag
 from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 
 def post_list(request):
@@ -17,17 +18,14 @@ def tag_list(request):
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
-    # def get(self, request, slug):
-    #     post = get_object_or_404(Post, slug__iexact=slug)
-    #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
-    # def get(self, request, slug):
-    #     tag = get_object_or_404(Tag, slug__iexact=slug)
-    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
-
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
