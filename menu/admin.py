4670e0777aa19048af296907b1906f33e915efd5
AlexanderVolk <mr.vol4onok@gmail.com>
2020-01-24 01:18:22 +0300

    Add admin panel to the menu app

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 from .models import Menu, MenuItem
 
 
-admin.site.register(Menu)
-admin.site.register(MenuItem)
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'published')
+    list_filter = ('published',)
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(MPTTModelAdmin):
+    list_display = ('title', 'name', 'parent', 'menu', 'sort', 'published')
+    list_filter = ('menu', 'parent', 'published')
+    search_fields = ('name', 'parent__name', 'menu__name')
+    save_as = True
+    list_editable = ('published',)
+    mptt_level_indent = 5
